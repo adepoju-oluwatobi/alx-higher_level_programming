aea508af25c1f048b042a035c43384767d289070
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-02 20:13:59 +0100

    finished mandatory

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * struct listint_s - A singly linked list
+ * @num: an integer
+ * @next: point to the next node
+ * Description: list node structure
+ */
+typedef struct listint_s
+{
+	int num;
+	struct listint_s *next;
+} listint_t;
+
 int check_cycle(listint_t *list);
 
 #endif
