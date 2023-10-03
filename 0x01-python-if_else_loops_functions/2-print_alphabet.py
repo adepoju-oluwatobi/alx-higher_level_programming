ee599556b74e322f064c9a15032070286749fa76
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-03 18:37:05 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,13 +1,3 @@
-# Start with the ASCII value of 'a'
-ascii_value = ord('a')
-
-# Iterate through the lowercase alphabet
-while ascii_value <= ord('z'):
-    # Convert the ASCII value back to a character and print it without a newline
-    print(chr(ascii_value), end='')
-
-    # Increment the ASCII value to move to the next character
-    ascii_value += 1
-
-# Print a newline character to start a new line after printing all characters
-print()
+#!/usr/bin/python3
+for i in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(i), end='')
