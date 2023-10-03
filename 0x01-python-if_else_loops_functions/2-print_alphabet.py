804eda24512a0e741c77a9b173dc4216de6764ff
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-03 18:49:23 +0100

    task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-output = ""
-
 for i in range(ord('a'), ord('z') + 1):
-    output += "{:c}".format(i)
-
-# Use print() only once to print the entire output with a newline character
-print(output)
+    print("{:c}".format(i), end='')
