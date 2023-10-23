9ad3264b3f9040e09684ad45e52c34ae0f6cf5ae
Adepoju Oluwatobi <kunletobi4@gmail.com>
2023-10-23 19:32:09 +0100

    edited

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    index = 0
+    i = 0
     while True:
         try:
-            if index < x:
-                print(my_list[index], end='')
-                index += 1
+            if i < x:
+                print(my_list[i], end='')
+                i += 1
             else:
                 print()
-                return index
+                return i
         except IndexError:
             print()
-            return index
+            return i
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
 
-    index = printed_ints = 0
+    i = printed_ints = 0
     while True:
         try:
-            if index < x:
-                print("{:d}".format(my_list[index]), end='')
-                index += 1
+            if i < x:
+                print("{:d}".format(my_list[i]), end='')
+                i += 1
                 printed_ints += 1
             else:
                 print()
                 return printed_ints
         except (ValueError, TypeError):
-            index += 1
+            i += 1
